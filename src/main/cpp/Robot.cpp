717f3b184f206ac4dd9f9c18aab729909456495f
bearbotics2358 <bearbotics2358@gmail.com>
2022-01-14 15:59:38 -0600

    Set up code base - cmm

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,32 @@
+
+#include "Robot.h"
+
+#include <fmt/core.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {}
+
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
