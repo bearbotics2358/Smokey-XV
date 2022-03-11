8bc27690cfe7c63ffdebeb7597e752b74a0bae35
Athryx <jack.x.roscoe@gmail.com>
2022-03-11 09:02:48 -0600

    added documentation to AbsoluteEncoder, fixed formatting in BeamBreak

--- src/main/include/AbsoluteEncoder.h
+++ src/main/include/AbsoluteEncoder.h
@@ -2,13 +2,35 @@
 
 #include <frc/AnalogInput.h>
 
+/** An encoder which remembers its position after power cycles.
+ *  The absolute encoder must be connected to an analog port on the rio.
+ *  The encoder will have a certain minimum voltage level and maximum voltage level,
+ *  and the voltage outputted by the encoder will vary linearly over this range as the encoder is turned from 0 to 360 degrees.
+ *  This minimum and maximum voltage level must be calibrated.
+ */
 class AbsoluteEncoder {
     public:
+        /** Constructor for absolute encoder, sets offset to 0 by default.
+         *  @param port the port number on the roborio that the encoder is plugged into.
+         *  @param minVolts the minimum amount of volts the encoder will output.
+         *  @param maxVolts the maximum amount of volts the encoder will output.
+         */
         AbsoluteEncoder(int port, double minVolts, double maxVolts);
-        // offset is in degrees
+
+        /** Constructor for absolute encoder.
+         *  Has a configurable offset, which is the offset angle that the encoder is mounted at on the robot.
+         *  This is useful if the encoder is rotated away from the desired 0 point.
+         *  @param port the port number on the roborio that the encoder is plugged into.
+         *  @param minVolts the minimum amount of volts the encoder will output.
+         *  @param maxVolts the maximum amount of volts the encoder will output.
+         *  @param offset the offset angle from 0 in rotations.
+         */
         AbsoluteEncoder(int port, double minVolts, double maxVolts, double offset);
 
+        /** Get the angle from the encoder in degrees. */
         double getAngle() const;
+
+        /** Get the angle from the encoder in rotations. */
         double getRotations() const;
 
     private:
--- src/main/include/BeamBreak.h
+++ src/main/include/BeamBreak.h
@@ -1,17 +1,14 @@
+#pragma once
 
 #include "Prefs.h"
 #include <frc/DigitalInput.h>
 
-class BeamBreak
-{
-public:
+class BeamBreak {
+    public:
+        BeamBreak(int port);
+        bool beamBroken();
+        bool isBroken = false;
 
-    BeamBreak(int port);
-    bool beamBroken();
-    bool isBroken = false; 
-
-private:    
-
-    frc::DigitalInput a_Input; 
-
+    private:
+        frc::DigitalInput a_Input;
 };
