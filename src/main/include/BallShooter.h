e92aa6a61e3dad0f551f06ba20fd15961907debe
Athryx <jack.x.roscoe@gmail.com>
2022-03-11 09:08:29 -0600

    documented BallShooter class

--- src/main/include/BallShooter.h
+++ src/main/include/BallShooter.h
@@ -4,20 +4,31 @@
 #include <ctre/Phoenix.h>
 #include <rev/CANSparkMax.h>
 
+/** Class for the shooter */
 class BallShooter {
     public:
+        /** Constructor for the shooter.
+         *  @param leftId can id of left shooter motor.
+         *  @param rightId can id of right shooter motor.
+         */
         BallShooter(int leftId, int rightId);
 
-        // sets speed in rpm
+        /** Set shooter speed in rpm.
+         *  The requested input is slowly scaled up to match the input rpm, which avoids damaging the motor if it spools up too fast.
+         *  Change the private member alpha to effect how fast this speeds up.
+         *  @param rpm desired shooter speed in rpm.
+         */
         void setSpeed(double rpm);
 
-        // gets speed in rpms
+        /** Gets the current shooter speed in rpm. */
         double getSpeed();
 
     private:
         TalonFX a_shooterLeft;
         TalonFX a_shooterRight;
 
+        // alpha and previous input are used to slowly scale the requested input to the desired speed,
+        // instead of immedeately changing the requested value for the shooter, which could be a problem due to the heavy flywheels.
         double alpha { 0.5 };
         double previousInput { 0.0 };
 };
