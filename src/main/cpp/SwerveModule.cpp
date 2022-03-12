66defa77f6353f91bfbbb82b1c64a534705ffc9a
Athryx <jack.x.roscoe@gmail.com>
2022-03-11 21:27:57 -0600

    removed unnesecary resetting of steer encoder to 0

--- src/main/cpp/SwerveModule.cpp
+++ src/main/cpp/SwerveModule.cpp
@@ -37,7 +37,6 @@ void SwerveModule::resetDriveEncoder() {
 }
 
 void SwerveModule::resetSteerEncoder() {
-    steerEncNEO.SetPosition(0);
     // need to subtract from 1 because the encoders face oppoosite direction
     double absAngle = 360.0 * (1.0 - absSteerEnc.getRotations());
     float relAngle = getRelativeAngle();
